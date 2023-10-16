f42d3e6940746f195a1280a721ea8fb03aada1f4
Pinto-PINTO <marie334fe@gmail.com>
2023-10-15 23:03:53 -0400

    Project Initiation

---
+++ app/src/main/java/com/example/testproject/Adapter/ToDoAdapter.java
@@ -0,0 +1,92 @@
+package com.example.testproject.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.testproject.MainActivity;
+import com.example.testproject.Model.ToDoModel;
+import com.example.testproject.R;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder>{
+    private List<ToDoModel> todoList;
+
+    private MainActivity activity;
+
+    public ToDoAdapter(MainActivity activity) {
+        this.activity = activity;
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_layout, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+
+    public void onBindViewHolder(ViewHolder holder, int position) {
+//        db.openDatabase();
+
+        ToDoModel item = todoList.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(toBoolean(item.getStatus()));
+//        holder.task.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    db.updateStatus(item.getId(), 1);
+//                } else {
+//                    db.updateStatus(item.getId(), 0);
+//                }
+//            }
+//        });
+    }
+
+    public int getItemCount() {
+        return todoList.size();
+    }
+
+//    public Context getContext() {
+//        return activity;
+//    }
+
+
+    private boolean toBoolean(int n) {
+        return n!=0;
+    }
+
+    public void setTasks(List<ToDoModel> todoList) {
+        this.todoList = todoList;
+        notifyDataSetChanged();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        CheckBox task;
+
+        ViewHolder(View view) {
+            super(view);
+            task = view.findViewById(R.id.todoCheckBox);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/testproject/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.testproject;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getSupportActionBar(); // Get the ActionBar
+        if (actionBar != null) {
+            actionBar.hide(); // Check for null before invoking methods
+        }
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+
+
+    }
+}
